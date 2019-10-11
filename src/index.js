a023307a4eedfd0c02cefa5fc282f87a0238a74f
tothricsaj <tothricsaj@gmail.com>
2019-10-11 20:20:49 +0200

    concept is not working......yet

--- src/index.js
+++ src/index.js
@@ -2,10 +2,25 @@ import "./main.scss";
 
 var el = document.getElementsByClassName('dd-box')[0];
 
-el.addEventListener('mousedown', (e) => {
-    el.style.borderColor = 'green';
-});
+document.addEventListener('mousemove', (e) => {
+    // console.log(`This is the coordinates: x -> ${e.screenX}, y -> ${e.screenY}`);
 
-el.addEventListener('mouseup', (e) => {
-    el.style.borderColor = 'orange';
+    let xCoor = e.screenX;
+    let yCoor = e.screenY;
+
+    el.addEventListener('mousedown', (e) => {
+        el.style.borderColor = 'green';
+
+        // console.log(`x->${xCoor}, y->${yCoor}`);
+
+        el.style.top = yCoor;
+        el.style.left = xCoor;
+    });
+
+    el.addEventListener('mouseup', (e) => {
+        el.style.borderColor = 'orange';
+
+        el.style.top = '0';
+        el.style.left = '0';
+    });
 });
