df4f777ffeeeac950159a2db0ef77b8be4909636
tothricsaj <tothricsaj@gmail.com>
2019-10-16 06:46:15 +0200

    remover button style

--- src/app/AddNote.js
+++ src/app/AddNote.js
@@ -3,22 +3,29 @@ import { addMove } from "./MoveNotes";
 let elTop = 60;
 let elLeft = 10;
 let zIndex = 0;
+let keyCount = 0;
 
 export default function AddnOte() {
-    var addBtn = document.getElementById('add-note');
+    let addBtn = document.getElementById('add-note');
+
     addBtn.addEventListener('click', (e) => {
         elTop += 5;
         elLeft += 5;
         zIndex++;
 
         let div = `
-            <div class="dd-box" style="top: ${elTop}px; left: ${elLeft}px; z-index: ${zIndex}">
-                <div class="dd-box__title">This is the title</div>
+            <div class="dd-box" key="${keyCount}" style="top: ${elTop}px; left: ${elLeft}px; z-index: ${zIndex}">
+                <div class="dd-box__title">
+                    This is the title
+                    <div class="dd-box__remover">x</div>
+                </div>
                 I still here :(
             </div>
         `;
         document.body.insertAdjacentHTML('afterbegin', div);
         addMove();
+
+        keyCount++;
     });
 }
 
