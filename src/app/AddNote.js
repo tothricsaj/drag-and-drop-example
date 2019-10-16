9b141179500792ac055efa195fdbf9ca97d83994
tothricsaj <tothricsaj@gmail.com>
2019-10-16 06:59:15 +0200

    added element can be deleteable

--- src/app/AddNote.js
+++ src/app/AddNote.js
@@ -14,16 +14,25 @@ export default function AddnOte() {
         zIndex++;
 
         let div = `
-            <div class="dd-box" key="${keyCount}" style="top: ${elTop}px; left: ${elLeft}px; z-index: ${zIndex}">
+            <div class="dd-box" data-key="${keyCount}" style="top: ${elTop}px; left: ${elLeft}px; z-index: ${zIndex}">
                 <div class="dd-box__title">
                     This is the title
-                    <div class="dd-box__remover">x</div>
+                    <div class="dd-box__remover" data-removerId="remover-${keyCount}">x</div>
                 </div>
                 I still here :(
             </div>
         `;
         document.body.insertAdjacentHTML('afterbegin', div);
         addMove();
+
+        let ddBox = document.querySelector(`[data-key="${keyCount}"]`);
+        let removerBtn = document.querySelector(`[data-removerId="remover-${keyCount}"]`);
+
+        removerBtn.addEventListener('click', (e) => {
+            document.body.removeChild(ddBox);
+        });
+
+        console.log(ddBox);
 
         keyCount++;
     });
